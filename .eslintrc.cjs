a09543264173ce208ff5abaf136bce4e1b46fab2
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-03-11 10:12:49 -0300

    chore(eslint-config): 🔧 extend linting rules for enhanced code quality
    - Add `plugin:sonarjs/recommended`, `plugin:compat/recommended`, `plugin:promise/recommended`, and `plugin:astro/jsx-a11y-strict` to the ESLint configuration.
    - Streamline the `overrides` section for `.astro` files, focusing on a unified parser configuration.
    - Remove specific `astro` rules from overrides to embrace broader and more comprehensive linting standards.
    - Update eslintignore
    - clean stettings.json comments
    
    chore(eslint,tsconfig): 🎨 pass format:fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,21 @@
 module.exports = {
-  extends: ['plugin:astro/recommended'],
-  plugins: ['prettier'],
-  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:astro/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:compat/recommended',
+    'plugin:promise/recommended',
+    'plugin:astro/jsx-a11y-strict',
+    'prettier',
+  ],
   overrides: [
     {
       files: ['*.astro'],
-      plugins: ['astro'],
-      env: {
-        node: true,
-        'astro/astro': true,
-        es2020: true,
-      },
-
       parser: 'astro-eslint-parser',
-
       parserOptions: {
         parser: '@typescript-eslint/parser',
         extraFileExtensions: ['.astro'],
       },
-      rules: {
-        'astro/no-conflict-set-directives': 'error',
-        'astro/no-unused-define-vars-in-style': 'error',
-      },
+      rules: {},
     },
   ],
 };
