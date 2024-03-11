07c3f9e2ea358734478ea2314bacd458b19b8013
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-03-11 11:27:03 -0300

    chore(prettier): 🔧 update from prettierrc to .prettierrc.mjs

---
+++ .prettierrc.mjs
@@ -0,0 +1,37 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: 'always',
+  semi: true,
+  tabWidth: 2,
+  printWidth: 140,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  endOfLine: 'crlf',
+  plugins: ['prettier-plugin-astro', '@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    '^@config$',
+    '^@assets/(.*)$',
+    '^@components/(.*)$',
+    '^@content/(.*)$',
+    '^@layouts/(.*)$',
+    '^@pages/(.*)$',
+    '^@data/(.*)$',
+    '^@styles/(.*)$',
+    '^@utils/(.*)$',
+    '^@/(.*)$',
+    '^.[./].*',
+    '',
+  ],
+  overrides: [
+    {
+      files: ['*.astro'],
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
+
+export default config;
